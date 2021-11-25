8ff0d67be754c34ce1ad4e887a61a45090d47c8e
Sandsculpture-xhy <iam_harry@163.com>
2021-11-25 16:15:27 -0600

    eee

---
+++ OI/USACO/2.3.3.cpp
@@ -0,0 +1,16 @@
+/*
+ID:iam_har1
+LANG:C++11
+TASK:zerosum
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    #ifndef LOCALJUDGE
+        freopen("zerosum.in","r",stdin);
+        freopen("zerosum.out","w",stdout);
+    #endif
+    return 0;
+}
