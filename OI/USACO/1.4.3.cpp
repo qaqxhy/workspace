4fdf7b0641e94f6aee586c28c3038906c309b337
Sandsculpture-xhy <iam_harry@163.com>
2021-12-22 08:44:51 -0600

    qwq

--- OI/USACO/1.4.3.cpp
+++ OI/USACO/1.4.3.cpp
@@ -6,14 +6,12 @@
 #define MAXSTALL 200
 int hascow[MAXSTALL];
 
-int
-intcmp(const void *va, const void *vb)
+int intcmp(const void *va, const void *vb)
 {
-	return *(int*)vb - *(int*)va;
+    return *(int *)vb - *(int *)va;
 }
 
-void
-main(void)
+void main(void)
 {
     FILE *fin, *fout;
     int n, m, nstall, ncow, i, j, c, lo, hi, nrun;
@@ -21,48 +19,50 @@ main(void)
 
     fin = fopen("barn1.in", "r");
     fout = fopen("barn1.out", "w");
-    
+
     assert(fin != NULL && fout != NULL);
 
     fscanf(fin, "%d %d %d", &m, &nstall, &ncow);
-    for(i=0; i<ncow; i++) {
-	fscanf(fin, "%d", &c);
-	hascow[c-1] = 1;
+    for (i = 0; i < ncow; i++)
+    {
+        fscanf(fin, "%d", &c);
+        hascow[c - 1] = 1;
     }
 
-    n = 0;	/* answer: no. of uncovered stalls */
+    n = 0; /* answer: no. of uncovered stalls */
 
     /* count empty stalls on left */
-    for(i=0; i<nstall && !hascow[i]; i++)
-	n++;
+    for (i = 0; i < nstall && !hascow[i]; i++)
+        n++;
     lo = i;
 
     /* count empty stalls on right */
-    for(i=nstall-1; i>=0 && !hascow[i]; i--)
-	n++;
-    hi = i+1;
+    for (i = nstall - 1; i >= 0 && !hascow[i]; i--)
+        n++;
+    hi = i + 1;
 
     /* count runs of empty stalls */
     nrun = 0;
     i = lo;
-    while(i < hi) {
-	while(hascow[i] && i<hi)
-	    i++;
+    while (i < hi)
+    {
+        while (hascow[i] && i < hi)
+            i++;
 
-	for(j=i; j<hi && !hascow[j]; j++)
-	    ;
+        for (j = i; j < hi && !hascow[j]; j++)
+            ;
 
-	run[nrun++] = j-i;
-	i = j;
+        run[nrun++] = j - i;
+        i = j;
     }
 
     /* sort list of runs */
     qsort(run, nrun, sizeof(run[0]), intcmp);
 
     /* uncover best m-1 runs */
-    for(i=0; i<nrun && i<m-1; i++)
-	n += run[i];
+    for (i = 0; i < nrun && i < m - 1; i++)
+        n += run[i];
 
-    fprintf(fout, "%d\n", nstall-n);
+    fprintf(fout, "%d\n", nstall - n);
     exit(0);
 }
