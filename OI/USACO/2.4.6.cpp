fe0943b716c96b0610715ad1a3da7fd62eafd33c
qaqxhy <iam_harry@163.com>
2022-04-12 19:10:56 -0400

    new submit

---
+++ OI/USACO/2.4.6.cpp
@@ -0,0 +1,29 @@
+/*
+ID:iam_har1
+LANG:C++11
+TASK:fracdec
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+int main()
+{
+    ios::sync_with_stdio(0);
+    #ifndef LOCALJUDGE
+        freopen("fracdec.in","r",stdin);
+        freopen("fracdec.out","w",stdout);
+    #endif
+    cin>>n>>m;
+    int t=sqrt(min(n,m));
+    for(int i=2;i<=t;i++)
+    {
+        if(n%i==0&&m%i==0)
+        {
+            n/=i;
+            m/=i;
+        }
+    }
+    double ans=n/m;
+    cout<<ans;
+    return 0;
+}
