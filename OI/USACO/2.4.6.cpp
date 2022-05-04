46ef3e18acb4bd7b78a1b85c24430f96d0a92d39
qaqxhy <iam_harry@163.com>
2022-05-04 19:11:27 -0400

    uodate

--- OI/USACO/2.4.6.cpp
+++ OI/USACO/2.4.6.cpp
@@ -3,47 +3,120 @@ ID:iam_har1
 LANG:C++11
 TASK:fracdec
 */
-#include <bits/stdc++.h>
+
+
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
 using namespace std;
-int n,m;
-int dec[114514];
-int decnt=0;
+
+const int maxn = 100010;
+int mod, d, x, y, cnt, t, pos, tot;
+char a[maxn], ans[maxn], c[maxn];
+int b[maxn];
+bool f[maxn], flag;
+
+void close()
+{
+    fclose(stdin);
+    fclose(stdout);
+    exit(0);
+}
+
+void work()
+{
+    while (1)
+    {
+        t = x;
+        x *= 10;
+        mod = x % y;
+        x = x / y;
+        if (f[t] == true || mod == 0)
+        {
+            if (mod == 0)
+            {
+                cnt++;
+                a[cnt] = x + 48;
+                flag = true;
+            }
+            break;
+        }
+        cnt++;
+        a[cnt] = x + 48;
+        b[cnt] = t;
+        f[t] = true;
+        x = mod;
+    }
+    /*
+    printf("t:%d\n",t);
+    for (int i=1;i<=cnt;i++)
+    cout<<b[i]<<" ";
+    cout<<endl;
+    */
+    for (int i = 1; i <= cnt; i++)
+        if (t == b[i])
+        {
+            pos = i;
+            break;
+        }
+    if (flag)
+    {
+        for (int i = 1; i <= cnt; i++)
+            printf("%c", a[i]);
+        cout << endl;
+        close();
+    }
+    tot = 0;
+    for (int i = 1; i <= cnt; i++)
+    {
+        if (pos == i)
+        {
+            tot++;
+            ans[tot] = '(';
+        }
+        tot++;
+        ans[tot] = a[i];
+    }
+    tot++;
+    ans[tot] = ')';
+    for (int i = 1; i <= tot; i++)
+    {
+        printf("%c", ans[i]);
+        if (i % 76 == 0)
+            printf("\n");
+    }
+    cout << endl;
+}
+
+void init()
+{
+    freopen("fracdec.in", "r", stdin);
+    freopen("fracdec.out", "w", stdout);
+    scanf("%d%d", &x, &y);
+    int d = x / y;
+    x = x - d * y;
+    cnt = 0;
+    while (1)
+    {
+        mod = d % 10;
+        d = d / 10;
+        cnt++;
+        c[cnt] = mod + 48;
+        if (d == 0)
+            break;
+    }
+    for (int i = 1; i <= cnt; i++)
+        a[i] = c[cnt - i + 1];
+    cnt++;
+    a[cnt] = '.';
+}
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    #ifndef LOCALJUDGE
-        freopen("fracdec.in","r",stdin);
-        freopen("fracdec.out","w",stdout);
-    #endif
-    cin>>n>>m;
-    int t=sqrt(min(n,m));
-    for(int i=2;i<=t;i++)
-    {
-        while(n%i==0&&m%i==0)
-        {
-            n/=i;
-            m/=i;
-        }
-    }
-    double ans=(double)n/(double)m;
-    if(ceil(ans)==ans)
-    {
-        cout<<ans<<".0";
-        return 0;
-    }
-    int in=ans;
-    ans-=in;
-    while(ans!=0)
-    {
-        ans*=10;
-        ::dec[++decnt]=ans;
-        int tmp=ans;
-        ans-=tmp;
-    }
-    cout<<in<<endl<<decnt<<endl;
-    for(int i=1;i<=decnt;i++)
-    {
-        cout<<::dec[i];
-    }
+    init();
+    work();
+    close();
     return 0;
 }
