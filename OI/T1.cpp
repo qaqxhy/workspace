789f2f2c48a03bc849b159e3c9bddf7720c90579
qaqxhy <iam_harry@163.com>
2022-06-22 22:16:37 -0400

    yeayeayea

--- OI/T1.cpp
+++ OI/T1.cpp
@@ -1,38 +1,16 @@
-// t1
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int n;
-char a[114514];
-int num = 0;
-bool temp=true;
 int main()
 {
-    scanf("%d", &n);
-    scanf("%s", a);
-    for (int ii = 0; ii < n / 2; ii++){
-        
-        if ((a[2 * ii] == 'G') && (a[2 * ii + 1] == 'H'))
+    int count=0;
+    for(int i=0;i<=6;i++)
+    {
+        for(int j=i+1;j<=7;j++)
         {
-            num = num+1;
-            temp = true;
-            break;
-        }
-        else if ((a[2 * ii] == 'H') && (a[2 * ii + 1] == 'G'))
-        {
-            temp=false;
-            break;
+            for(int k=j+1;k<=8;k++)
+            {
+                for(int l=k+1;l<=9;l++) count++;
+            }
         }
     }
-    for (int ii = 0; ii < n / 2; ii++){
-        if ((a[2 * ii] == 'G') && (a[2 * ii + 1] == 'H') && (!temp))
-        {
-            num = num+2;
-            temp = true;
-        }
-        else if ((a[2 * ii] == 'H') && (a[2 * ii + 1] == 'G') && (temp))
-        {
-            temp=false;
-        }
-    }
-    cout << num << endl;
 }
