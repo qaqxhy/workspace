a69dbca44b204b98c6b3ec4b02583b85ae0ec5de
qaqxhy <iam_harry@163.com>
2022-06-23 20:27:19 -0400

    hh

--- OI/T1.cpp
+++ OI/T1.cpp
@@ -1,16 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int count=0;
-    for(int i=0;i<=6;i++)
+    int count = 0;
+    for (int i = 0; i <= 6; i++)
     {
-        for(int j=i+1;j<=7;j++)
+        for (int j = i + 1; j <= 7; j++)
         {
-            for(int k=j+1;k<=8;k++)
+            for (int k = j + 1; k <= 8; k++)
             {
-                for(int l=k+1;l<=9;l++) count++;
+                for (int l = k + 1; l <= 9; l++)
+                {
+                    cout<<i<<j<<k<<l<<"\n";
+                    count++;
+                }
             }
         }
     }
+    cout << count;
 }
+//Y9FAF6251217HF7FD0B6DF8D95CBCC5B
